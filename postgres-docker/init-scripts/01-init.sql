acc982375a9f4c775c47671b6933b6f2910069cd
vijaykumarn <vijaykumarn>
2025-12-24 11:38:01 +0100

    add postgres docker compose file with init scripts

---
+++ postgres-docker/init-scripts/01-init.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE students
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    age INTEGER
+);
+
---
+++ postgres-docker/init-scripts/02-seed-data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO students
+    (name, age)
+VALUES
+    ('Mkyong', 40),
+    ('Ali', 28),
+    ('Teoh', 18);
